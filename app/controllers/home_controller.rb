a87a9c50e7b5830dcf444177572be13ab30fd593
Shaan Patel <shaanpat@gmail.com>
2019-04-17 16:41:12 -0700

    add privacy policy and terms of service pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/contact'
+  get 'home/faq'
+  get 'home/privacy_policy'
+  get 'home/terms_of_service'
   
   root 'home#index'
 end
