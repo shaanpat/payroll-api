e4e89554a776f30c0058066b1b2e2d7d448c0f69
Shaan Patel <shaanpat@gmail.com>
2019-04-18 22:57:07 -0700

    create get-started link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'about', :to => 'home#about'
+  get 'faq', :to => 'home#faq'
+  get 'get-started', :to => 'accounts#new'
   get 'privacy-policy', :to => 'home#privacy_policy'
   get 'terms-of-service', :to => 'home#terms_of_service'
-  get 'faq', :to => 'home#faq'
 
   resources :accounts
   
